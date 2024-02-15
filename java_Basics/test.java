9b85b06d97cb1b84e694665c23f3da76075f0de0
Irfan Saeed <132193755+IrfanSaeednarejo@users.noreply.github.com>
2024-02-15 19:43:10 +0000

    test done

---
+++ java_Basics/test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class test{
+    public static void main(String [] args){
+        System.out.println("HElloe World");
+
+        System.out.println("dnsjkfdnmcv");
+        System.out.println("ncmx,vnm,lfg");
+
+        Scanner inp= new Scanner(System.in);
+        int x=inp.nextInt();
+        System.out.println(x+" it is the Answer");
+    }
+}
