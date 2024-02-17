a03dda343fd58b6917ea47db7a5aac957918f8ee
Irfan Saeed <132193755+IrfanSaeednarejo@users.noreply.github.com>
2024-02-17 14:32:13 +0000

    update done

--- java_Basics/test.java
+++ java_Basics/test.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class test{
     public static void main(String [] args){
-        System.out.println("HElloe World");
+        System.out.println("HEllo World");
 
-        System.out.println("dnsjkfdnmcv");
-        System.out.println("ncmx,vnm,lfg");
+        System.out.println("testing the github codespaces");
+        System.out.println("Working Excellent");
 
         Scanner inp= new Scanner(System.in);
         int x=inp.nextInt();
-        System.out.println(x+" it is the Answer");
+        System.out.println(x+" it is the Answer in github codespaces");
     }
 }
