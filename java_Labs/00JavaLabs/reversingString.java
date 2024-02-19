c7a226eb609d915d971ac38a243d7ccb2fa9f767
Irfan Saeed <132193755+IrfanSaeednarejo@users.noreply.github.com>
2024-02-19 18:48:01 +0000

    String's reversing added

---
+++ java_Labs/00JavaLabs/reversingString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ class Rev
+{
+    public static void main(String args[])
+    {
+        
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter the string : ");
+        String st = obj.nextLine();
+       revfunc(st);
+    }
+     static void revfunc(String ar)
+    {
+        String re="";
+        for(int i=ar.length();i>0; --i)
+        {
+            re=re+(ar.charAt(i-1));
+        }
+        System.out.println("Reverse string : ");
+
+        for(int i=0;i<ar.length(); ++i)
+        {
+            System.out.print(""+re.charAt(i));
+        }
+    }
+
+}
